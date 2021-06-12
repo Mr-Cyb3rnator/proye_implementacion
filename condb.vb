4f444c4938b834d93bca00947f2ae4c2a95fb183
Eduard Figueroa <ejosue_fm@hotmail.com>
2021-06-11 19:36:16 -0600

    Initial commit

---
+++ condb.vb
@@ -0,0 +1,23 @@
+﻿Module condb
+
+    Public conectar As New SqlClient.SqlConnection
+
+    Public cn As New SqlClient.SqlConnection("Data Source=localhost;Initial Catalog=base_proyecto;Integrated Security=True")
+    Public cmd As New SqlClient.SqlCommand
+    Public adt As New SqlClient.SqlDataAdapter
+    Public ds As New DataSet
+    Public sql As String
+
+    Public Sub conecta()
+
+        conectar.ConnectionString = ("Data Source=localhost;Initial Catalog=base_proyecto;Integrated Security=True")
+        conectar.Open()
+
+
+    End Sub
+
+
+
+
+
+End Module
