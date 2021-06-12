638a47f8ca2b34d033fdaef73a724161e9db922f
dilcia21 <dilciagissele@gmail.com>
2021-06-12 00:11:57 -0600

    menu
    cambio en la interfaz de menú

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -49,113 +49,119 @@ Partial Class Form3
         'lblprivilegio
         '
         Me.lblprivilegio.AutoSize = True
-        Me.lblprivilegio.Location = New System.Drawing.Point(10, 7)
+        Me.lblprivilegio.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblprivilegio.Location = New System.Drawing.Point(295, 9)
         Me.lblprivilegio.Name = "lblprivilegio"
-        Me.lblprivilegio.Size = New System.Drawing.Size(38, 15)
+        Me.lblprivilegio.Size = New System.Drawing.Size(63, 22)
         Me.lblprivilegio.TabIndex = 9
-        Me.lblprivilegio.Text = "Menu"
+        Me.lblprivilegio.Text = "MENU"
+        Me.lblprivilegio.TextAlign = System.Drawing.ContentAlignment.TopRight
         '
         'Button4
         '
-        Me.Button4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button4.BackColor = System.Drawing.Color.BurlyWood
         Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
-        Me.Button4.Location = New System.Drawing.Point(5, 130)
+        Me.Button4.Location = New System.Drawing.Point(28, 190)
         Me.Button4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(144, 61)
+        Me.Button4.Size = New System.Drawing.Size(127, 110)
         Me.Button4.TabIndex = 8
         Me.Button4.UseVisualStyleBackColor = False
         '
         'Button3
         '
-        Me.Button3.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
-        Me.Button3.Location = New System.Drawing.Point(463, 33)
+        Me.Button3.BackColor = System.Drawing.Color.BurlyWood
+        Me.Button3.BackgroundImage = Global.proye.My.Resources.Resources.dieta
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button3.Location = New System.Drawing.Point(470, 41)
         Me.Button3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(144, 61)
+        Me.Button3.Size = New System.Drawing.Size(127, 110)
         Me.Button3.TabIndex = 7
         Me.Button3.UseVisualStyleBackColor = False
         '
         'Button2
         '
-        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
-        Me.Button2.Location = New System.Drawing.Point(231, 33)
+        Me.Button2.BackColor = System.Drawing.Color.BurlyWood
+        Me.Button2.Image = Global.proye.My.Resources.Resources.icons8_año_del_buey_48
+        Me.Button2.Location = New System.Drawing.Point(248, 41)
         Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(144, 61)
+        Me.Button2.Size = New System.Drawing.Size(127, 110)
         Me.Button2.TabIndex = 6
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Button1
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.Location = New System.Drawing.Point(5, 33)
+        Me.Button1.BackColor = System.Drawing.Color.BurlyWood
+        Me.Button1.BackgroundImage = Global.proye.My.Resources.Resources.vaca__6_
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button1.Location = New System.Drawing.Point(28, 41)
         Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(144, 60)
+        Me.Button1.Size = New System.Drawing.Size(127, 110)
         Me.Button1.TabIndex = 5
         Me.Button1.UseVisualStyleBackColor = False
         '
         'Button5
         '
-        Me.Button5.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button5.Image = CType(resources.GetObject("Button5.Image"), System.Drawing.Image)
-        Me.Button5.Location = New System.Drawing.Point(231, 130)
+        Me.Button5.BackColor = System.Drawing.Color.BurlyWood
+        Me.Button5.Image = Global.proye.My.Resources.Resources.ventas
+        Me.Button5.Location = New System.Drawing.Point(248, 190)
         Me.Button5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(144, 61)
+        Me.Button5.Size = New System.Drawing.Size(127, 110)
         Me.Button5.TabIndex = 10
         Me.Button5.UseVisualStyleBackColor = False
         '
         'Button6
         '
-        Me.Button6.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button6.BackColor = System.Drawing.Color.BurlyWood
         Me.Button6.Image = CType(resources.GetObject("Button6.Image"), System.Drawing.Image)
-        Me.Button6.Location = New System.Drawing.Point(463, 130)
+        Me.Button6.Location = New System.Drawing.Point(470, 190)
         Me.Button6.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(144, 61)
+        Me.Button6.Size = New System.Drawing.Size(127, 110)
         Me.Button6.TabIndex = 11
         Me.Button6.UseVisualStyleBackColor = False
         '
         'Button7
         '
-        Me.Button7.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button7.BackColor = System.Drawing.Color.BurlyWood
         Me.Button7.Image = CType(resources.GetObject("Button7.Image"), System.Drawing.Image)
-        Me.Button7.Location = New System.Drawing.Point(5, 230)
+        Me.Button7.Location = New System.Drawing.Point(28, 339)
         Me.Button7.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(144, 61)
+        Me.Button7.Size = New System.Drawing.Size(127, 110)
         Me.Button7.TabIndex = 12
         Me.Button7.UseVisualStyleBackColor = False
         '
         'Button8
         '
-        Me.Button8.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button8.BackColor = System.Drawing.Color.BurlyWood
         Me.Button8.Image = CType(resources.GetObject("Button8.Image"), System.Drawing.Image)
-        Me.Button8.Location = New System.Drawing.Point(231, 230)
+        Me.Button8.Location = New System.Drawing.Point(248, 339)
         Me.Button8.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(144, 61)
+        Me.Button8.Size = New System.Drawing.Size(127, 110)
         Me.Button8.TabIndex = 13
         Me.Button8.UseVisualStyleBackColor = False
         '
         'Button9
         '
-        Me.Button9.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button9.Image = CType(resources.GetObject("Button9.Image"), System.Drawing.Image)
-        Me.Button9.Location = New System.Drawing.Point(463, 230)
+        Me.Button9.BackColor = System.Drawing.Color.BurlyWood
+        Me.Button9.Image = Global.proye.My.Resources.Resources.pagar
+        Me.Button9.Location = New System.Drawing.Point(470, 339)
         Me.Button9.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(144, 61)
+        Me.Button9.Size = New System.Drawing.Size(127, 110)
         Me.Button9.TabIndex = 14
         Me.Button9.UseVisualStyleBackColor = False
         '
         'GroupBox1
         '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DarkSeaGreen
         Me.GroupBox1.Controls.Add(Me.Label9)
         Me.GroupBox1.Controls.Add(Me.Label8)
         Me.GroupBox1.Controls.Add(Me.Label7)
@@ -174,93 +180,101 @@ Partial Class Form3
         Me.GroupBox1.Controls.Add(Me.Button5)
         Me.GroupBox1.Controls.Add(Me.Button3)
         Me.GroupBox1.Controls.Add(Me.Button4)
-        Me.GroupBox1.Location = New System.Drawing.Point(22, 24)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 33)
         Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.GroupBox1.Size = New System.Drawing.Size(625, 304)
+        Me.GroupBox1.Size = New System.Drawing.Size(630, 461)
         Me.GroupBox1.TabIndex = 15
         Me.GroupBox1.TabStop = False
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(514, 213)
+        Me.Label9.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(498, 316)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(55, 15)
+        Me.Label9.Size = New System.Drawing.Size(81, 21)
         Me.Label9.TabIndex = 23
         Me.Label9.Text = "Compras"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(268, 213)
+        Me.Label8.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(283, 313)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(37, 15)
+        Me.Label8.Size = New System.Drawing.Size(58, 21)
         Me.Label8.TabIndex = 22
         Me.Label8.Text = "Gasto"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(44, 213)
+        Me.Label7.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(57, 313)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(47, 15)
+        Me.Label7.Size = New System.Drawing.Size(74, 21)
         Me.Label7.TabIndex = 21
-        Me.Label7.Text = "Valance"
+        Me.Label7.Text = "Balance"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(514, 113)
+        Me.Label6.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(498, 161)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(49, 15)
+        Me.Label6.Size = New System.Drawing.Size(72, 21)
         Me.Label6.TabIndex = 20
         Me.Label6.Text = "Clientes"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(268, 113)
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(278, 161)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(41, 15)
+        Me.Label5.Size = New System.Drawing.Size(65, 21)
         Me.Label5.TabIndex = 19
         Me.Label5.Text = "Ventas"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(44, 113)
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(42, 161)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(72, 15)
+        Me.Label4.Size = New System.Drawing.Size(108, 21)
         Me.Label4.TabIndex = 18
         Me.Label4.Text = "Ingredientes"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(514, 17)
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(502, 18)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 15)
+        Me.Label3.Size = New System.Drawing.Size(59, 21)
         Me.Label3.TabIndex = 17
         Me.Label3.Text = "Dietas"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(252, 16)
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(235, 18)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(154, 20)
+        Me.Label2.Size = New System.Drawing.Size(166, 21)
         Me.Label2.TabIndex = 16
         Me.Label2.Text = "Cabeza de ganado"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(44, 16)
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(57, 18)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(45, 15)
+        Me.Label1.Size = New System.Drawing.Size(66, 21)
         Me.Label1.TabIndex = 15
         Me.Label1.Text = "Grupos"
         '
@@ -269,13 +283,13 @@ Partial Class Form3
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.BurlyWood
-        Me.ClientSize = New System.Drawing.Size(700, 338)
+        Me.ClientSize = New System.Drawing.Size(654, 505)
         Me.Controls.Add(Me.lblprivilegio)
         Me.Controls.Add(Me.GroupBox1)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form3"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form3"
+        Me.Text = "Menú"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
