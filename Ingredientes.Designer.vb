b44a3e97583b86f0270a0d7a17cf9431e47aed53
Oscar Andrade <oandrade57@gmail.com>
2021-06-15 12:02:05 -0600

    Modificacion Form Ingredientes
    Validando campos en los textboxs
    Campo de Dieta no crash la BD
    Existen ErrorProviders pero falta

--- Ingredientes.Designer.vb
+++ Ingredientes.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Ingredientes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btneliminar = New System.Windows.Forms.Button()
         Me.btnguardar = New System.Windows.Forms.Button()
         Me.dgingredientes = New System.Windows.Forms.DataGridView()
@@ -31,23 +32,27 @@ Partial Class Ingredientes
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtcoddieta = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.bteditar = New System.Windows.Forms.Button()
+        Me.tt_Ingrediente = New System.Windows.Forms.ToolTip(Me.components)
+        Me.txtcoddieta = New System.Windows.Forms.TextBox()
+        Me.ep_Ingredientes = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.btnatras = New System.Windows.Forms.Button()
         CType(Me.dgingredientes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ep_Ingredientes, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btneliminar
         '
         Me.btneliminar.BackColor = System.Drawing.Color.BurlyWood
+        Me.btneliminar.Enabled = False
         Me.btneliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btneliminar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.btneliminar.Image = Global.proye.My.Resources.Resources.iconfinder_4781838_cancel_close_delete_exit_logout_icon_32px1
         Me.btneliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.btneliminar.Location = New System.Drawing.Point(328, 224)
-        Me.btneliminar.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btneliminar.Location = New System.Drawing.Point(375, 299)
         Me.btneliminar.Name = "btneliminar"
-        Me.btneliminar.Size = New System.Drawing.Size(118, 49)
+        Me.btneliminar.Size = New System.Drawing.Size(135, 65)
         Me.btneliminar.TabIndex = 20
         Me.btneliminar.Text = "Eliminar"
         Me.btneliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -56,14 +61,14 @@ Partial Class Ingredientes
         'btnguardar
         '
         Me.btnguardar.BackColor = System.Drawing.Color.BurlyWood
+        Me.btnguardar.Enabled = False
         Me.btnguardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnguardar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.btnguardar.Image = Global.proye.My.Resources.Resources.iconfinder_1031520_arrow_download_save_guardar_icon_32px1
         Me.btnguardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.btnguardar.Location = New System.Drawing.Point(80, 224)
-        Me.btnguardar.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnguardar.Location = New System.Drawing.Point(91, 299)
         Me.btnguardar.Name = "btnguardar"
-        Me.btnguardar.Size = New System.Drawing.Size(118, 49)
+        Me.btnguardar.Size = New System.Drawing.Size(135, 65)
         Me.btnguardar.TabIndex = 19
         Me.btnguardar.Text = "Guardar"
         Me.btnguardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
@@ -73,45 +78,46 @@ Partial Class Ingredientes
         '
         Me.dgingredientes.AllowUserToAddRows = False
         Me.dgingredientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgingredientes.Location = New System.Drawing.Point(376, 44)
-        Me.dgingredientes.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.dgingredientes.Location = New System.Drawing.Point(430, 59)
+        Me.dgingredientes.MultiSelect = False
         Me.dgingredientes.Name = "dgingredientes"
+        Me.dgingredientes.ReadOnly = True
         Me.dgingredientes.RowHeadersWidth = 51
         Me.dgingredientes.RowTemplate.Height = 28
-        Me.dgingredientes.Size = New System.Drawing.Size(433, 160)
+        Me.dgingredientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgingredientes.Size = New System.Drawing.Size(495, 213)
         Me.dgingredientes.TabIndex = 18
         '
         'txtcodingre
         '
-        Me.txtcodingre.Location = New System.Drawing.Point(186, 44)
-        Me.txtcodingre.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtcodingre.Enabled = False
+        Me.txtcodingre.Location = New System.Drawing.Point(213, 59)
         Me.txtcodingre.Name = "txtcodingre"
-        Me.txtcodingre.Size = New System.Drawing.Size(107, 23)
+        Me.txtcodingre.Size = New System.Drawing.Size(122, 27)
         Me.txtcodingre.TabIndex = 17
         '
         'txtcosto
         '
-        Me.txtcosto.Location = New System.Drawing.Point(182, 166)
-        Me.txtcosto.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtcosto.Enabled = False
+        Me.txtcosto.Location = New System.Drawing.Point(208, 221)
         Me.txtcosto.Name = "txtcosto"
-        Me.txtcosto.Size = New System.Drawing.Size(111, 23)
+        Me.txtcosto.Size = New System.Drawing.Size(126, 27)
         Me.txtcosto.TabIndex = 16
         '
         'txtdescripingre
         '
-        Me.txtdescripingre.Location = New System.Drawing.Point(186, 83)
-        Me.txtdescripingre.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtdescripingre.Location = New System.Drawing.Point(213, 111)
         Me.txtdescripingre.Name = "txtdescripingre"
-        Me.txtdescripingre.Size = New System.Drawing.Size(107, 23)
+        Me.txtdescripingre.Size = New System.Drawing.Size(122, 27)
         Me.txtdescripingre.TabIndex = 15
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(12, 166)
+        Me.Label3.Location = New System.Drawing.Point(14, 221)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(60, 21)
+        Me.Label3.Size = New System.Drawing.Size(72, 23)
         Me.Label3.TabIndex = 14
         Me.Label3.Text = "Costo:"
         '
@@ -119,9 +125,9 @@ Partial Class Ingredientes
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(12, 81)
+        Me.Label2.Location = New System.Drawing.Point(14, 108)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(104, 21)
+        Me.Label2.Size = New System.Drawing.Size(128, 23)
         Me.Label2.TabIndex = 13
         Me.Label2.Text = "Descripción:"
         '
@@ -129,52 +135,73 @@ Partial Class Ingredientes
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(12, 44)
+        Me.Label1.Location = New System.Drawing.Point(14, 59)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(168, 21)
+        Me.Label1.Size = New System.Drawing.Size(204, 23)
         Me.Label1.TabIndex = 12
         Me.Label1.Text = "Código Ingrediente:"
-        '
-        'txtcoddieta
-        '
-        Me.txtcoddieta.Location = New System.Drawing.Point(186, 125)
-        Me.txtcoddieta.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.txtcoddieta.Name = "txtcoddieta"
-        Me.txtcoddieta.Size = New System.Drawing.Size(107, 23)
-        Me.txtcoddieta.TabIndex = 22
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(12, 125)
+        Me.Label4.Location = New System.Drawing.Point(14, 167)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(119, 21)
+        Me.Label4.Size = New System.Drawing.Size(145, 23)
         Me.Label4.TabIndex = 21
         Me.Label4.Text = "Código Dieta:"
         '
         'bteditar
         '
         Me.bteditar.BackColor = System.Drawing.Color.BurlyWood
+        Me.bteditar.Enabled = False
         Me.bteditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.bteditar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.bteditar.Image = Global.proye.My.Resources.Resources.iconfinder_185042_edit_modify_icon_32px1
         Me.bteditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.bteditar.Location = New System.Drawing.Point(204, 224)
-        Me.bteditar.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.bteditar.Location = New System.Drawing.Point(233, 299)
         Me.bteditar.Name = "bteditar"
-        Me.bteditar.Size = New System.Drawing.Size(118, 49)
+        Me.bteditar.Size = New System.Drawing.Size(135, 65)
         Me.bteditar.TabIndex = 27
         Me.bteditar.Text = "Editar"
         Me.bteditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.bteditar.UseVisualStyleBackColor = False
         '
+        'tt_Ingrediente
+        '
+        Me.tt_Ingrediente.IsBalloon = True
+        Me.tt_Ingrediente.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.tt_Ingrediente.ToolTipTitle = "Tipo Datos"
+        '
+        'txtcoddieta
+        '
+        Me.txtcoddieta.Enabled = False
+        Me.txtcoddieta.Location = New System.Drawing.Point(213, 167)
+        Me.txtcoddieta.Name = "txtcoddieta"
+        Me.txtcoddieta.Size = New System.Drawing.Size(122, 27)
+        Me.txtcoddieta.TabIndex = 22
+        '
+        'ep_Ingredientes
+        '
+        Me.ep_Ingredientes.ContainerControl = Me
+        '
+        'btnatras
+        '
+        Me.btnatras.BackgroundImage = Global.proye.My.Resources.Resources.icons8_flecha_responder_a_todos_48
+        Me.btnatras.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnatras.Location = New System.Drawing.Point(654, 299)
+        Me.btnatras.Name = "btnatras"
+        Me.btnatras.Size = New System.Drawing.Size(94, 63)
+        Me.btnatras.TabIndex = 28
+        Me.btnatras.UseVisualStyleBackColor = True
+        '
         'Ingredientes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DarkSeaGreen
-        Me.ClientSize = New System.Drawing.Size(844, 309)
+        Me.ClientSize = New System.Drawing.Size(965, 412)
+        Me.Controls.Add(Me.btnatras)
         Me.Controls.Add(Me.bteditar)
         Me.Controls.Add(Me.txtcoddieta)
         Me.Controls.Add(Me.Label4)
@@ -187,11 +214,11 @@ Partial Class Ingredientes
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Ingredientes"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ingredientes"
         CType(Me.dgingredientes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ep_Ingredientes, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -206,7 +233,10 @@ Partial Class Ingredientes
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents txtcoddieta As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents bteditar As Button
+    Friend WithEvents tt_Ingrediente As ToolTip
+    Friend WithEvents txtcoddieta As TextBox
+    Friend WithEvents ep_Ingredientes As ErrorProvider
+    Friend WithEvents btnatras As Button
 End Class
--- Ingredientes.vb
+++ Ingredientes.vb
@@ -2,10 +2,13 @@
 
 Public Class Ingredientes
 
+    Dim loadingForm As Boolean
+
+
     Private Sub cargargrid()
 
         conecta()
-        Dim cargar_datos_clientes As String = "select * from ingredientes "
+        Dim cargar_datos_clientes As String = "SELECT cod_ingredientes AS 'Código', descripcion AS 'Ingrediente', costo AS 'Costo', cod_dieta AS '# Dieta' FROM ingredientes "
         Dim mostrar As New DataTable
 
         Using adpmostrar As New SqlDataAdapter(cargar_datos_clientes, conectar)
@@ -23,23 +26,36 @@ Public Class Ingredientes
 
     Private Sub btnguardar_Click(sender As Object, e As EventArgs) Handles btnguardar.Click
 
+        If (txtdescripingre.Text IsNot "" And txtcosto.Text IsNot "" And txtcoddieta.Text IsNot "") Then
+
+            Try
+                conecta()
+                Dim insertar_ingredientes As String = "insert into ingredientes(descripcion,costo,cod_dieta)values(@descripcion,@costo,@cod_dieta)"
+                Dim insertar As New SqlCommand(insertar_ingredientes, conectar)
+                insertar.Parameters.AddWithValue("@descripcion", txtdescripingre.Text)
+                insertar.Parameters.AddWithValue("@costo", txtcosto.Text)
+                insertar.Parameters.AddWithValue("@cod_dieta", txtcoddieta.Text)
+                insertar.ExecuteNonQuery()
+
+                conectar.Close()
+            Catch ex As Exception
+                MsgBox("Revise los Valores del Campo Dieta")
+                conectar.Close()
+            End Try
 
 
+            loadingForm = False
+            cargargrid()
 
-        conecta()
-        Dim insertar_ingredientes As String = "insert into ingredientes(descripcion,costo,cod_dieta)values(@descripcion,@costo,@cod_dieta)"
-        Dim insertar As New SqlCommand(insertar_ingredientes, conectar)
-        insertar.Parameters.AddWithValue("@descripcion", txtdescripingre.Text)
-        insertar.Parameters.AddWithValue("@costo", txtcosto.Text)
-        insertar.Parameters.AddWithValue("@cod_dieta", txtcoddieta.Text)
-
-        insertar.ExecuteNonQuery()
-
-
-
-        conectar.Close()
-        cargargrid()
-
+            dgingredientes.ClearSelection()
+            txtcodingre.Text = ""
+            txtdescripingre.Text = ""
+            txtcoddieta.Text = ""
+            txtcosto.Text = ""
+            loadingForm = True
+        Else
+            MsgBox("Campos no pueden quedar vacios")
+        End If
 
 
         'dgingredientes.Rows.Add(txtcodingre.Text, txtdescripingre.Text, txtcoddieta.Text, txtcosto.Text)
@@ -51,8 +67,13 @@ Public Class Ingredientes
     End Sub
 
     Private Sub Ingredientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        loadingForm = False
         cargargrid()
+        dgingredientes.ClearSelection()
+        loadingForm = True
+
+        tt_Ingrediente.SetToolTip(bteditar, "Seleccione una Fila para Editar")
+        tt_Ingrediente.SetToolTip(btneliminar, "Seleccione una Fila para Eliminar")
 
 
 
@@ -66,14 +87,28 @@ Public Class Ingredientes
     End Sub
 
     Private Sub btneliminar_Click(sender As Object, e As EventArgs) Handles btneliminar.Click
-
+        tt_Ingrediente.SetToolTip(btneliminar, "")
         conecta()
         Dim eliminar As String = "delete from ingredientes  where cod_ingredientes=@cod_ingredientes"
         Dim procesar As New SqlCommand(eliminar, conectar)
         procesar.Parameters.AddWithValue("@cod_ingredientes", txtcodingre.Text)
         procesar.ExecuteNonQuery()
         conectar.Close()
+
+
+        loadingForm = False
         cargargrid()
+        dgingredientes.ClearSelection()
+
+        txtcodingre.Text = ""
+        txtdescripingre.Text = ""
+        txtcoddieta.Text = ""
+        txtcosto.Text = ""
+
+        loadingForm = True
+
+        btneliminar.Enabled = False
+        bteditar.Enabled = False
 
 
 
@@ -81,7 +116,7 @@ Public Class Ingredientes
     End Sub
 
     Private Sub bteditar_Click(sender As Object, e As EventArgs) Handles bteditar.Click
-
+        tt_Ingrediente.SetToolTip(bteditar, "")
         conecta()
 
         Dim datos_ingredientes As String = "update ingredientes set descripcion=@descripcion,costo=@costo,cod_dieta=@cod_dieta where cod_ingredientes=@cod_ingredientes"
@@ -93,7 +128,22 @@ Public Class Ingredientes
 
         actualizar.ExecuteNonQuery()
         conectar.Close()
+
+        loadingForm = False
         cargargrid()
+        dgingredientes.ClearSelection()
+
+        txtcodingre.Text = ""
+        txtdescripingre.Text = ""
+        txtcoddieta.Text = ""
+        txtcosto.Text = ""
+
+        loadingForm = True
+
+
+        bteditar.Enabled = False
+        btneliminar.Enabled = False
+
 
 
 
@@ -102,24 +152,27 @@ Public Class Ingredientes
 
     Private Sub dgingredientes_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgingredientes.CellContentClick
         ' Dim row As DataGridViewRow = dgingredientes.CurrentRow
+
     End Sub
 
 
 
-    'Private Sub btnatras_Click(sender As Object, e As EventArgs)
-    'Me.Hide()
-    '    Form3.Show()
-    'End Sub
+
 
     Private Sub txtcodingre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtcodingre.KeyPress
         If Char.IsNumber(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcodingre, "")
         ElseIf Char.IsControl(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcodingre, "")
         ElseIf Char.IsSeparator(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcodingre, "")
         Else
             e.Handled = True
+            tt_Ingrediente.SetToolTip(txtcodingre, "Solo Acepta Numeros")
+
 
         End If
     End Sub
@@ -127,12 +180,16 @@ Public Class Ingredientes
     Private Sub txtcoddieta_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtcoddieta.KeyPress
         If Char.IsNumber(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcoddieta, "")
         ElseIf Char.IsControl(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcoddieta, "")
         ElseIf Char.IsSeparator(e.KeyChar) Then
+            tt_Ingrediente.SetToolTip(txtcoddieta, "")
             e.Handled = False
         Else
             e.Handled = True
+            tt_Ingrediente.SetToolTip(txtcoddieta, "Solo Acepta Numeros")
 
         End If
     End Sub
@@ -140,39 +197,160 @@ Public Class Ingredientes
     Private Sub txtcosto_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtcosto.KeyPress
         If Char.IsNumber(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcosto, "")
         ElseIf Char.IsControl(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcosto, "")
         ElseIf Char.IsSeparator(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtcosto, "")
         Else
             e.Handled = True
-
+            tt_Ingrediente.SetToolTip(txtcosto, "Solo Acepta Numeros")
         End If
     End Sub
 
     Private Sub txtdescripingre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtdescripingre.KeyPress
         If Char.IsLetter(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtdescripingre, "")
         ElseIf Char.IsControl(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtdescripingre, "")
         ElseIf Char.IsSeparator(e.KeyChar) Then
             e.Handled = False
+            tt_Ingrediente.SetToolTip(txtdescripingre, "")
         Else
             e.Handled = True
+            tt_Ingrediente.SetToolTip(txtdescripingre, "Solo Acepta Caracteres")
 
         End If
     End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
-    End Sub
 
-    Private Sub btnatras_Click(sender As Object, e As EventArgs)
-        Me.Hide()
+    Private Sub btnatras_Click(sender As Object, e As EventArgs) Handles btnatras.Click
+        Me.Close()
         Form3.Show()
     End Sub
 
     Private Sub txtcodingre_TextChanged(sender As Object, e As EventArgs) Handles txtcodingre.TextChanged
 
+    End Sub
+
+    Private Sub txtdescripingre_TextChanged(sender As Object, e As EventArgs) Handles txtdescripingre.TextChanged
+        If (txtdescripingre.Text IsNot "") Then
+            txtcoddieta.Enabled = True
+        Else
+            txtcoddieta.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtcoddieta_TextChanged(sender As Object, e As EventArgs) Handles txtcoddieta.TextChanged
+        If (txtcoddieta.Text IsNot "") Then
+            txtcosto.Enabled = True
+        Else
+            txtcosto.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtcosto_TextChanged(sender As Object, e As EventArgs) Handles txtcosto.TextChanged
+        If (txtcosto.Text IsNot "") Then
+            btnguardar.Enabled = True
+        Else
+            btnguardar.Enabled = False
+        End If
+    End Sub
+
+
+
+    Private Sub txtdescripingre_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtdescripingre.Validating
+        Dim estado As Boolean
+
+        estado = String.IsNullOrEmpty(txtdescripingre.Text)
+        If (estado) Then
+
+            ' e.Cancel = True
+            ep_Ingredientes.SetError(txtdescripingre, "Campo No Puede Quedar Vacio")
+
+        Else
+
+            ' e.Cancel = False
+            ep_Ingredientes.SetError(txtdescripingre, "")
+
+        End If
+
+    End Sub
+
+    Private Sub txtcoddieta_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtcoddieta.Validating
+        Dim estado As Boolean
+
+        estado = String.IsNullOrEmpty(txtcoddieta.Text)
+        If (estado) Then
+            '  e.Cancel = True
+            ep_Ingredientes.SetError(txtcoddieta, "Campo No Puede Quedar Vacio")
+        Else
+            ' e.Cancel = False
+            ep_Ingredientes.SetError(txtcoddieta, "")
+        End If
+    End Sub
+
+    Private Sub txtcosto_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtcosto.Validating
+        Dim estado As Boolean
+
+        estado = String.IsNullOrEmpty(txtcosto.Text)
+        If (estado) Then
+            ' e.Cancel = True
+            ep_Ingredientes.SetError(txtcosto, "Campo No Puede Quedar Vacio")
+        Else
+            ' e.Cancel = False
+            ep_Ingredientes.SetError(txtcosto, "")
+        End If
+    End Sub
+
+    Private Sub dgingredientes_SelectionChanged(sender As Object, e As EventArgs) Handles dgingredientes.SelectionChanged
+        If (loadingForm) Then
+
+            Dim fila As Integer
+            fila = dgingredientes.CurrentRow.Index
+
+
+            bteditar.Enabled = True
+            btneliminar.Enabled = True
+
+            txtcodingre.Text = dgingredientes.Rows(fila).Cells(0).Value
+            txtdescripingre.Text = dgingredientes.Rows(fila).Cells(1).Value
+            txtcosto.Text = dgingredientes.Rows(fila).Cells(2).Value
+            txtcoddieta.Text = dgingredientes.Rows(fila).Cells(3).Value
+
+            btnguardar.Enabled = False
+            txtdescripingre.Enabled = True
+            txtcosto.Enabled = True
+            txtcoddieta.Enabled = True
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub bteditar_MouseHover(sender As Object, e As EventArgs) Handles bteditar.MouseHover
+
+    End Sub
+
+    Private Sub bteditar_MouseMove(sender As Object, e As MouseEventArgs) Handles bteditar.MouseMove
+        If (bteditar.Enabled <> True) Then
+            tt_Ingrediente.SetToolTip(bteditar, "Seleccione una Fila para Editar")
+        Else
+            tt_Ingrediente.SetToolTip(bteditar, "")
+        End If
+    End Sub
+
+    Private Sub bteditar_MouseUp(sender As Object, e As MouseEventArgs) Handles bteditar.MouseUp
+        If (bteditar.Enabled <> True) Then
+            tt_Ingrediente.SetToolTip(bteditar, "Seleccione una Fila para Editar")
+        Else
+            tt_Ingrediente.SetToolTip(bteditar, "")
+        End If
     End Sub
 End Class
